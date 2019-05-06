bba1072e2aee2d9cf6d1f27937fba627ffd0fa6a
Padam Sandhu <sandhp7@gmail.com>
2019-05-06 14:39:16 -0400

    Update index.js

--- node-web-app-docker-image/index.js
+++ node-web-app-docker-image/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hi there!!');
+  res.send('Hi there!! I am edited');
 });
 
 app.listen(8080, () => console.log('Listening on port 8080'));
